2589e4e43821dc88c6a124434a8e90e78d51be2b
JioxoFab <JioxoFab@users.noreply.github.com>
2016-04-08 21:01:08 +0200

    add object wit password requirements

--- src/app/controllers/createAccount.js
+++ src/app/controllers/createAccount.js
@@ -1,3 +1,35 @@
 angular.module('myApp').controller('createAccountCtrl', function() {
 
+	$scope.passwordCriteria = [
+		{
+			id: 1,
+			description: 'Password contains at least 8 characters.',
+			regEx: '^.{8,}$'
+		},
+		{
+			id: 2,
+			description: 'Password contains at least two upper-case characters.',
+			regEx: ''
+		},
+		{
+			id: 3,
+			description: 'Password contains at least two lower-case characters.',
+			regEx: ''
+		},
+		{
+			id: 4,
+			description: 'Password contains at least one digit.',
+			regEx: ''
+		},
+		{
+			id: 5,
+			description: 'Password contains at least one special character.',
+			regEx: ''
+		}
+	];
+
+	$scope.checkPassword = function() {
+		// Check all the password requirements.
+	};
+
 });
